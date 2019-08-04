c60afadef8ae987ea560371d621ebf9505c7a700
Ruben Buitelaar <rubenbuitelaar@gmail.com>
2019-08-04 21:05:43 +0200

    Updates

---
+++ precache-manifest.74c30c29e849ea63d795356133d0939e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1f57a5a30ab491ffe180",
+    "url": "/static/js/main.1f57a5a3.chunk.js"
+  },
+  {
+    "revision": "57768cbd88555fd2178b",
+    "url": "/static/js/2.57768cbd.chunk.js"
+  },
+  {
+    "revision": "1f57a5a30ab491ffe180",
+    "url": "/static/css/main.ede173e3.chunk.css"
+  },
+  {
+    "revision": "3f8c21cd98929a2e20737316fd1a413d",
+    "url": "/index.html"
+  }
+];
